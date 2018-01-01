751340aa54bdd15fceb910314c6849af49848f1f
seohae <kimseohae96@naver.com>
2018-01-01 17:59:16 +0900

    01/01 ch02 공부중

--- 02/ 파이썬 프로그래밍 기초,자료형/List_01.py
+++ 02/ 파이썬 프로그래밍 기초,자료형/List_01.py
@@ -4,9 +4,3 @@ b = [1, 2, 3] #숫자를 요소값으로 가짐
 c = ['Life', 'is', 'too', 'short'] #문자열을 요소값으로 가짐
 d = [1, 2, 'Life', 'is'] #숫자와 문자열을 함께 요소값으로 가짐
 e = [1, 2, ['Life', 'is']] #리스트 자체를 요소값으로 가짐
-
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
