f8602eeb536cd44a382e26020271c905cdedd3f8
seohae <kimseohae96@naver.com>
2018-01-02 13:10:51 +0900

    01/02 튜플 공부완료, 딕셔너리 공부시작

---
+++ 02/ 파이썬 프로그래밍 기초,자료형/dictionary/dictionary_01.py
@@ -0,0 +1,9 @@
+# 딕셔너리의 예
+# 딕셔너리 : {key1:value1, key2:value2 ...}
+# Key와 Value의 쌍 여러개와 {,}로 둘러싸여있다
+# Key:변하지 않는 값, Value:변하는 값
+
+dic = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
+
+# Key: 'name', 'phone', 'birth'
+# Value: 'pey', '0119993323', '1118'
