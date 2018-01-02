da32c776ce5a135e57cc0f2a5d5b5e42ac8b946a
seohae <kimseohae96@naver.com>
2018-01-02 13:16:52 +0900

    01/02 튜플 공부완료, 딕셔너리 공부시작

--- 02/ 파이썬 프로그래밍 기초,자료형/dictionary/dictionary_01.py
+++ 02/ 파이썬 프로그래밍 기초,자료형/dictionary/dictionary_01.py
@@ -3,7 +3,8 @@
 # Key와 Value의 쌍 여러개와 {,}로 둘러싸여있다
 # Key:변하지 않는 값, Value:변하는 값
 
-dic = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
+dictionary = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
+print(dictionary)
 
 # Key: 'name', 'phone', 'birth'
 # Value: 'pey', '0119993323', '1118'
